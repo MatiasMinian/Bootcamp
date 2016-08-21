fb5ceb3fb8fa72af98015f76216b6f1048ebcea6
Matias Minian <minian.matias@gmail.com>
2016-08-21 10:26:16 -0300

    Small changes in blogging app

--- Day 2/blogging/src/main/java/com/bootcamp/app/User.java
+++ Day 2/blogging/src/main/java/com/bootcamp/app/User.java
@@ -1,36 +1,32 @@
 package com.bootcamp.app;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 public class User {
-	
+
 	private String username;
-	private SortedSet<Post> posts = new TreeSet<>();
-	
+	private List<Post> posts = new ArrayList<>();
+
 	public User(String username) {
 		this.username = username;
 	}
-	
+
 	public Post createPost(String title, String text, Set<String> tags) {
 		Post post = new Post(title, text, tags);
-		posts.add(post);
+		posts.add(0, post);;
 		if (tags != null) {
 			addTags(tags);
 		}
 		return post;
 	}
-	
+
 	public void deletePost(Post post) {
-		if (posts.remove(post)) {
-			System.out.println("Post deleted.");
-		} else {
-			System.out.println("Post wasn't deleted");
-		}
+		posts.remove(post);
 	}
-	
+
 	public void showRecentPosts(int quantity) {
 		Iterator<Post> it = posts.iterator();
 		for (int i = 0; i < quantity; i++) {
@@ -40,11 +36,11 @@ public class User {
 			it.next().show();
 		}
 	}
-	
+
 	public void addTags(Set<String> tags) {
-		TagsManager.getInstance().addTags(tags);		
+		TagsManager.getInstance().addTags(tags);
 	}
-	
+
 	/* *** GETTERS & SETTERS *** */
 
 	public String getUsername() {
@@ -55,11 +51,11 @@ public class User {
 		this.username = username;
 	}
 
-	public SortedSet<Post> getPosts() {
+	public List<Post> getPosts() {
 		return posts;
 	}
 
-	public void setPosts(SortedSet<Post> posts) {
+	public void setPosts(List<Post> posts) {
 		this.posts = posts;
 	}
 }
