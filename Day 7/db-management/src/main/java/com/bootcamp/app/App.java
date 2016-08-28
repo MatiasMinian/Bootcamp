fc7919c6b15500e8277ce368ab2c3f52648678a6
Matias Minian <minian.matias@gmail.com>
2016-08-28 12:02:11 -0300

    Read query from command line. Add methods to update price by five in catalog and to remove a supplier

--- Day 7/db-management/src/main/java/com/bootcamp/app/App.java
+++ Day 7/db-management/src/main/java/com/bootcamp/app/App.java
@@ -5,8 +5,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -61,10 +63,43 @@ public class App {
 			e.printStackTrace();
 		}
 		
-		
-		
-		
-		
+		Scanner scanner = new Scanner(System.in);
+		loop: while (true) {
+			System.out.println("1: Query");
+			System.out.println("2: Update Supplier");
+			System.out.println("3: Update Part");
+			System.out.println("4: Exit");
+			int option = scanner.nextInt();
+			scanner.nextLine();
+			switch (option) {
+			case 1:
+				System.out.println("Write query");
+				String query = scanner.nextLine();
+				try {
+					resultSet = statement.executeQuery(query);
+					ResultSetMetaData rsmd = resultSet.getMetaData();
+				    int columnsNumber = rsmd.getColumnCount();
+				    while (resultSet.next()) {
+				        for (int i = 1; i <= columnsNumber; i++) {
+				            if (i > 1) System.out.print(",  ");
+				            String columnValue = resultSet.getString(i);
+				            System.out.print(columnValue + " " + rsmd.getColumnName(i));
+				        }
+				        System.out.println("");
+				    }
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			default:
+				break loop;
+			};
+		}
+		scanner.close();	
 		
 		try {
 			resultSet.close();
@@ -76,10 +111,35 @@ public class App {
 		}
 	}
 	
-	public void updatePrice(int catalogId, Connection connect) throws SQLException {
+	public static void updatePriceBy5(int catalogId, Connection connect) {
 		Statement statement = null;
-		statement = connect.createStatement();
-		statement.executeQuery("");
-				
+		ResultSet resultSet = null;
+		try {
+			statement = connect.createStatement();
+			resultSet = statement.executeQuery("select cost from bootcamp_suppliers.catalog where id = " + catalogId);
+			resultSet.next();
+			int newCost = resultSet.getInt("cost") + 5;
+			statement.executeUpdate("update bootcamp_suppliers.catalog set cost = " + newCost + " where id = " + catalogId);
+			System.out.println("New cost: " + newCost);	
+			statement.close();
+			resultSet.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	public static void removeSupplier(int supplierId, Connection connect) {
+		Statement statement = null;
+		try {
+			statement = connect.createStatement();
+			statement.executeUpdate("delete from bootcamp_suppliers.catalog where sid = " + supplierId);
+			statement.executeUpdate("delete from bootcamp_suppliers.suppliers where sid = " + supplierId);
+			statement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
 	}
 }
