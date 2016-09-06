88dfb93b6e4bc5f0d868bebbb18d3090e43f2c28
Matias Minian <minian.matias@gmail.com>
2016-09-06 09:06:19 -0300

    Create project for iBuy

---
+++ MainApp/iBuy/src/main/java/com/bootcamp/app/IBuyApplication.java
@@ -0,0 +1,12 @@
+package com.bootcamp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IBuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IBuyApplication.class, args);
+	}
+}
