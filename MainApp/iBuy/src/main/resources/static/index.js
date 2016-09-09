b12b7e9e5f5f1cef7c3c2038b77b80ad25f7bce5
Matias Minian <minian.matias@gmail.com>
2016-09-08 21:18:36 -0300

    model index.html for users. Populate table with data.

--- MainApp/iBuy/src/main/resources/static/index.js
+++ MainApp/iBuy/src/main/resources/static/index.js
@@ -1,11 +1,22 @@
 $(function(){
-	$("#user_button").click(function(){
-		$.get("http://localhost:8080/api/users", function(data, status){
-			$.each(data, function(index, user){
-				$("#main_data").append(user.username)
-			})
-			//$("#main_data").text(JSON.stringify(data));	
-			alert(status);
-		}, "json")		
-	})	
+	$.get("http://localhost:8080/api/users", function(data, status){
+		var mainTableBody = $("#main_table_body");
+		$.each(data, function(index, user){
+			var buttonStatus = "";
+			if (user.deleteable) {
+				buttonStatus = "disabled"								
+			}
+			mainTableBody.append("<tr><td><button type=\"button\" class=\"btn btn-primary\">Impersonate</button></td>\
+				<td>"+user.username+"</td><td>"+user.firstName+"</td><td>"+user.lastName+"</td>\
+				<td><button type=\"button\" class=\"btn btn-default\">Edit</button></td>\
+				<td><button type=\"button\" class=\"btn btn-danger "+buttonStatus+"\">Delete</button></td></tr>"
+			);
+		});
+	}, "json");		
+});
+
+$(function(){
+	$("#button_create_user").click(function(){
+		
+	});
 });
